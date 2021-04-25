edeaddff2d8fd19f29bb95e64b9d4a3cd4af6cb9
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-04-25 19:32:41 +0700

    fix class to className property on NotFound page

--- src/pages/errors/NotFound.js
+++ src/pages/errors/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NotFound() {
     return (
         <div className="w-full bg-white border border-solid border-gray-100 rounded-lg p-5">
-            <h1 class="text-2xl mb-3">Not Found</h1>
+            <h1 className="text-2xl mb-3">Not Found</h1>
             <p>
                 Halaman tidak ditemukan
             </p>
